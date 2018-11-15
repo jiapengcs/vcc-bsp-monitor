b90f9f6469dba436b19a7b898255939ef4591216
Jiapengcs <jiapengkang@gmail.com>
2018-11-15 20:39:23 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # app name, must be modified!(used for directory name, so it cannot contains special symbols)
-APP_NAME=foobar
+APP_NAME=vcc-bsp-monitor
 ARTIFACTS_NAME=artifacts.tar.gz
 BASE_PATH=~/deploy/$APP_NAME
 TARGET_PATH=$BASE_PATH/target
